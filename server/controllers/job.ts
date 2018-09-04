cdb23c4307af1cef2ed0d429cddd927db4d5cadb
Wiyanto Ngasinur <wngasinur@gmail.com>
2018-09-04 22:49:15 +0800

    Fixed path

--- server/controllers/job.ts
+++ server/controllers/job.ts
@@ -2,11 +2,11 @@ import { Request, Response } from 'express';
 import { asyncMiddleware } from '../util/asyncMiddleware';
 
 import { logger } from '../util/log';
-import { syncCondoMaster } from 'jobs/condo_master';
-import { syncMapPolygons } from 'jobs/map_polygons';
-import { syncCondoMasterLoc } from 'jobs/condo_master_loc';
-import { syncSalesHistory } from 'jobs/sales_history';
-import { syncAggSalesHistory } from 'jobs/agg_sales_history';
+import { syncCondoMaster } from '../jobs/condo_master';
+import { syncMapPolygons } from '../jobs/map_polygons';
+import { syncCondoMasterLoc } from '../jobs/condo_master_loc';
+import { syncSalesHistory } from '../jobs/sales_history';
+import { syncAggSalesHistory } from '../jobs/agg_sales_history';
 
 
 export let condoMaster = asyncMiddleware(async (req: Request, res: Response) => {
